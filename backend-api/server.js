dbd8a35c74514a4a7fbdeea6d7738ff628199340
Benhoward2000 <benhoward_2000@yahoo.com>
2025-10-01 17:31:06 -0700

    Update server.js duplicate express message

--- backend-api/server.js
+++ backend-api/server.js
@@ -1,9 +1,6 @@
 // Load environment variables from .env
-require("dotenv").config();
 
-const express = require("express");// Load environment variables from .env
 require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const session = require("express-session");
